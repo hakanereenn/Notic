64252b49aa9ea88413cad1ee0ba7c8e0704d53d7
flyboy12 <53239096+flyboy12@users.noreply.github.com>
2021-09-30 15:41:11 +0300

    yeniden aktarılacak

--- notes_app/lib/presentation/routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static final String home = '/home';
-  static final String addNote = '/add-note';
-  static final String readNote = '/read-note';
-  static final String setting = '/setting';
-  static final String editNote = '/edit-note';
-  static final String detailTagList = '/detail-tag-list';
-}
